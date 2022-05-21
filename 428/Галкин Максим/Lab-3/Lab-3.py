0c854d8da11e7b1a53f0dea1ca46ec99951eaf06
Ziker0k <94623900+Ziker0k@users.noreply.github.com>
2022-05-21 14:49:28 +0300

    Update Lab-3.py

--- 428/Галкин Максим/Lab-3/Lab-3.py
+++ 428/Галкин Максим/Lab-3/Lab-3.py
@@ -1,28 +1,31 @@
 from sympy import *
-
 x = Symbol('x')
 
 print("Вариант - ",len("ГалкинМаксимЕвгеньевич")%5)
 
-def Metod_ProstoiIteracii(eps, y, dy):
-    x0 = 10.0 #Начальная точка
-    c = 1000.0 #Произвольная вспомогательная переменная для условия заканчивания цикла
-    i = 0 #Счётчик
-    d = 0.0
-    
-    while(abs(c-x0) >= eps):
+def Cycle_func(x0,c,eps,y,dy,i):
+    if(abs(c-x0) >= eps):
         d = x0 - float(y(x0)/dy(x0))  #Расчет последующих точек с постоянным пересчетом производной
         c = x0 #предыдущая точка
         x0 = d #Фиксация новой точки
         i += 1
+        return Cycle_func(x0,c,eps,y,dy,i)
+    else:
+        return x0,i
 
-    print ("Корень -", x0)
-    print ("Итерации -", i)
-    
+def Metod_ProstoiIteracii(eps, f):
+    x0 = 10.0 #Начальная точка
+    c = 1000.0 #Произвольная вспомогательная переменная для условия заканчивания цикла
+    i = 0 #Счётчик
+    y = lambdify(x, f)
+    dy = lambdify(x, diff(f, x))
+        
+    return Cycle_func(x0,c,eps,y,dy,i)  
+     
 f = log(x+1) - x**2 #Уравнение
 
-y = lambdify(x, f)
-dy = lambdify(x, diff(f, x))
-
 eps = float(input("Введите точность: "))
-Metod_ProstoiIteracii(eps, y, dy)
+res,count = Metod_ProstoiIteracii(eps, f)
+print("x = ",res)
+print("Итерации - ",count)
+
