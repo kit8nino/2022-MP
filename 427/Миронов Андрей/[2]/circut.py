16407294567c9da0bd42fa0e6f1259bec470929f
AndrewMironov13 <96263076+AndrewMironov13@users.noreply.github.com>
2022-06-03 12:17:20 +0300

    Add files via upload

---
+++ 427/Миронов Андрей/[2]/circut.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#сделали Миронов и Поздоровкин
+import scipy.signal
+import numpy as np
+import math as m
+import matplotlib.pyplot as plt
+from numpy.fft import rfft, rfftfreq, fft, ifft
+
+class Circut():
+    def __init__(self, y_n,t,f_0):
+        self.a=1
+        self.y_n=y_n
+        self.t=t
+        self.f_0=f_0
+        self.buf=list()
+        self.sig=list()
+    def convol(self):
+        x_n=np.arange(0,self.t,1./self.f_0)
+        w=np.angle(np.exp((complex(-(1/2),8)*x_n)))
+        self.buf.append(self.y_n)
+        self.buf.append(self.y_n*w)
+        self.sig=self.y_n*w
+        return(self.y_n*w)
+    def Get_Buf(self,a):
+        if a<0:
+            return self.bufх[0]
+        return self.buf[1]
+    def Return_Ar(self):
+        return self.buf[1]
+    def No_Change(self):
+        return self.y_n
+    def Return_Gener(self):
+        for i in self.buf[0]:
+            yield 
+
