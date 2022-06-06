edb837bd8a5febef800e227fb505ce834de56032
Gringua <96185587+Gringua@users.noreply.github.com>
2022-06-06 19:06:38 +0300

    Add files via upload

---
+++ 428/Клоканов Артём/[2] Lab/analyzer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Класс анализатора
+class Analyzer():
+    pass
+
---
+++ 428/Клоканов Артём/[2] Lab/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Основной код
+import gen_simple
+import gen_heir
+import analyzer
+import circuit
+
