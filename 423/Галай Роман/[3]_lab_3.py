e65d1e564a45d57e5f3f729860de31a0399510cd
RomanGalay <roma29102456@gmail.com>
2022-06-06 15:41:55 +0300

    [3] Update

--- 423/Галай Роман/[3]_lab_3.py
+++ 423/Галай Роман/[3]_lab_3.py
@@ -4,22 +4,22 @@ import cmath as mt
 print('Вариант №',len('Галай Роман Олегович')%5)
 f=lambda x: mt.log(x)+(x+1)**3
 def differ(func, x: float, epsilon: float):
-return (func(x + epsilon / 2.) - func(x - epsilon / 2.)) \
-/ epsilon
+    return (func(x + epsilon / 2.) - func(x - epsilon / 2.)) \
+           / epsilon
 
 
 def newton(func, x: float, epsilon: float):
-try:
-div = differ(func, x, epsilon)
-if (div == 0.):
-return newton(func, (x + epsilon / 2 * (-1) ** (random.random())).real, epsilon)
-new_x = x - func(x) / div
-if abs(new_x - x) > epsilon:
-return newton(func, new_x, epsilon)
-else:
-return new_x
-except:
-return x
+    try:
+        
+        if ((differ(func, x, epsilon)) == 0.):
+            return newton(func, (x + epsilon / 2 * (-1) ** (random.random())).real, epsilon)
+        
+        if abs((x - func(x) / (differ(func, x, epsilon)))- x) > epsilon:
+            return newton((func, x - func(x) / (differ(func, x, epsilon))), epsilon)
+        else:
+            return (x - func(x) / (differ(func, x, epsilon)))
+    except:
+        return x
 
 epsilon=float(input('Точность ввода: '))
 approx=newton(f,1.,epsilon)
