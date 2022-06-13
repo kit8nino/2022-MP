a983c6a8627fa3009ee337fbd918fa3a13ac019a
kit8nino <freezer.nik@gmail.com>
2022-06-13 12:29:24 +0300

    [0] fixes

--- 428/Петров Илья/[2]lab/generator_simple.py
+++ 428/Петров Илья/[2]lab/generator_simple.py
@@ -1,4 +1,4 @@
-mport numpy as np
+import numpy as np
 import math as m
 import matplotlib.pyplot as plt
 
--- 429/Каланцов Андрей/lab_2/analizer.py
+++ 429/Каланцов Андрей/lab_2/analizer.py
@@ -4,7 +4,6 @@ import numpy as np
 
 
 class Analizer:
-    ''' Класс анализатора сигнала '''
 
     def __init__(self, signal: Generator):
         self.signal = signal
