4a093440608ead3841c038e26f7db5f8160a93df
Anton Plakhov <80921438+JulyTheGnome@users.noreply.github.com>
2022-09-12 21:14:38 +0300

    L[2] & L[3]

--- 426/Плахов Антон/L3.py
+++ 426/Плахов Антон/L3.py
@@ -5,24 +5,19 @@ import numpy as np
 def f(x):
     return (2-x)*np.exp(x)
 
-def c(X,x):
-    return (X+x)/2
-
-def solution(X,x,a):
-    if abs(f(c(X,x)-f(X)))<E:
-        print("x = ",c(X,x))
-    elif(f(c(X,x)*a)>0):
-        solution(c(X,x),x,a)
-    else:
-        solution(X,c(X,x),a)
-    return()
-
 #main
 print("Введите точность измерений")
 
 E=float(input())
+x1=1
+x2=3
 a=1
-X=1
-x=3
+while abs(f(x2)-f(x1))>E:
+    if(f(a)>0):
+        x1=a
+        a=(x1+x2)/2
+    else:
+        x2=a
+        a=(x1+x2)/2
 
-solution(X,x,a)
+print("x = ",a)
