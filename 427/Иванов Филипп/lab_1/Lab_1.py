c6f5df94c08b5fd83cf0c495f80ccb7d9f9bf408
Philipp54 <96289687+Philipp54@users.noreply.github.com>
2022-09-13 19:49:21 +0300

    Update Lab_1.py

--- 427/Иванов Филипп/lab_1/Lab_1.py
+++ 427/Иванов Филипп/lab_1/Lab_1.py
@@ -1,6 +1,5 @@
 
 import numpy as np
-#Точки для сокровища ставятся автоматически (рандомно, вместе с проверкой) 
 def Maze_Reader():
     y_max=0
     maze=[]
@@ -11,7 +10,6 @@ def Maze_Reader():
         x_max, y_max = len(maze[0]), len(maze)
     return x_max,y_max, maze
 
-#print(Maze_Reader())
 
 max_x,max_y,maze=Maze_Reader()
 maze_list= list(maze)
@@ -19,17 +17,14 @@ POSSIBLE_WAYS = ('N', 'S', 'W', 'E')
 
 
 
-#Ставит символ sign в лабиринте по координате coord
 def set_point(coord, sign):
     global maze_list
 
-    #Преобразовываем строку в список для модификации
     maze_row = list(maze_list[coord[1]])
     maze_row[coord[0]] = sign
     maze_list[coord[1]] = ''.join(maze_row)
 
 
-#Находит координаты первой пустой точки в ряду
 def get_point(maze, row):
     return [maze[row].find(" "), row]
 
@@ -106,44 +101,40 @@ def cut_way_back(direction):
         return ('N', 'S', 'W')
 
 
-#Восстанавливает путь до сокровища от точки coord
+
 def restore_path(coord):
     global maze_list, path_to_exit
     
     for node in path_to_exit:
         set_point(coord, '.')
-        #Определяет следующую координату
+        
         coord = step(coord, node)
 
 count_2l_ways = 0 
 def find_a_way(maze, coord, possible_ways):
     global path_to_exit, current_path, count_2l_ways
     
-    #Проверка границ лабиринта
-    #if not is_coord_in_maze(maze, coord):
-     #   print('not in bounds!')
-      #  return
+  
 
-    #Проверка выхода из лабиринта 
+    
     if is_coord_exit(coord):
         print('exit')
         return
 
-    #Проверка на наход сокровища
+   
     if is_coord_treasure(coord):
         print('finding a new way')
         path_to_exit = current_path.copy()
         return
 
-    #Отбрасываем пути, которые не являются кратчайшими
+    
     if len(current_path) > len(path_to_exit):
-        #print('too large path')
-        #print(f'{len(current_path)} > {len(path_to_exit)}')
+        
         count_2l_ways+=1
         
         return
 
-    #Рекурсивный поиск по доступным направлениям
+   
     for direction in possible_ways:
         if is_path_clean(maze, step(coord, direction)):
                current_path.append(direction)
@@ -153,16 +144,13 @@ def find_a_way(maze, coord, possible_ways):
     return 
     
 
-#past_way хранит координаты точки, из которой пришли
+
 past_way = [[-1 for j in range(max_x)] for i in range(max_y)] # инициализация
 
 
 
 def find_the_exit(treasure, end):
-    #treasure[0] - column, treasure[1] - row
-    #y - row, x - column
-
-    #Инициализируем очередь и заносим туда координаты сокровища
+    
     queue = []
     queue.append(treasure)
 
@@ -187,21 +175,20 @@ def find_the_exit(treasure, end):
     x = end[0]
     y = end[1]  
     
-    #В список past_way передаём координаты выхода(поиск в ширину), а затем мы забираем координаты x,y в отдельные
-    #переменные и перезаписывам их
+ 
     while past_way[y][x] != 0:
         set_point([x,y], ',')
         temp_x = past_way[y][x][0]
         temp_y = past_way[y][x][1]
         x, y = temp_x, temp_y
-        #print(past_way[y][x])
+       
 
 path_to_exit = []
 for i in range(len(maze)*len(maze[0])):
     path_to_exit.append(' ')
 current_path = []
 
-#Получение стартовой и конечной точек
+
 start_point = get_point(maze, 0)
 end_point = get_point(maze, max_y-1)
 print("start:", start_point)
@@ -209,26 +196,26 @@ print("end:",end_point)
 
 treasure_is_here = ()
 
-#Чтобы сокровище не спавнилось в стене
+
 while(not (treasure_is_here and is_path_clean(maze, treasure_is_here))):
     x = np.random.randint(40,85) #int(input(f'Where is threasure? (x is between 0 and {max_x-1}) '))
     y = np.random.randint(90,190) #int(input(f'Where is threasure? (y is between 0 and {max_y-1}) '))
     treasure_is_here = (abs(x) if abs(x) < max_x-1 else max_x-1, abs(y) if abs(y) < max_y-1 else max_y-1)
 
-#Отмечаем сокровище на карте
+
 set_point(treasure_is_here, '*')
 print("treasure is here:",treasure_is_here)
 
-#Ищем путь до сокровища
+
 find_a_way(maze, start_point, POSSIBLE_WAYS)
 
-#Восстанавливаем путь до сокровища
+
 restore_path(start_point)
 
-#Ищем выход 
+
 find_the_exit(treasure_is_here, end_point)
 
-#Записываем в файл
+
 f = open('maze-for-me-done.txt', 'w')
 for i in range(len(maze_list)):
     f.write(maze_list[i])
